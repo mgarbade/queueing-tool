09098c003e6a9519e030c2b578a036457b816979
Martin Garbade <garbade.work@gmail.com>
2024-11-08 15:04:00 +0100

    fix: escape backlash in strings

--- queue/block_parser.py
+++ queue/block_parser.py
@@ -47,7 +47,7 @@ class Block_Parser(object):
         block[0] = re.sub('gpu=true', 'gpus=1', block[0])
         block[0] = re.sub('gpu=false', 'gpus=0', block[0])
         # extract job parameters
-        meta = re.sub('\).*', '', re.sub('.*\(', '', block[0])).split(',')
+        meta = re.sub('\\).*', '', re.sub('.*\\(', '', block[0])).split(',')
         parsed_block = Block()
         for param in meta:
             if len(param.split('=')) == 2:
@@ -64,7 +64,7 @@ class Block_Parser(object):
     def parse(self, script):
         try:
             f = open(script, 'r')
-            content = f.read().split('\n')
+            content = f.read().split('\\n')
             f.close()
         except:
             print('Parser: script %s could not be opened.' % script)
