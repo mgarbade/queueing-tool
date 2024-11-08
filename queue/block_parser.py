0d6dcf77305768dde9696f4948725f37902a2101
Martin Garbade <garbade.work@gmail.com>
2024-11-08 15:14:02 +0100

    block_parser - more printing

--- queue/block_parser.py
+++ queue/block_parser.py
@@ -3,10 +3,9 @@
 import re
 
 class Block(object):
-
     def __init__(self):
         self.values = dict()
-        self.values['name'] = 'unkown-job'
+        self.values['name'] = 'unknown-job'  # Corrected typo from 'unkown-job' to 'unknown-job'
         self.values['threads'] = 1
         self.values['memory'] = 1024
         self.values['gpus'] = 0
@@ -16,48 +15,46 @@ class Block(object):
 
     def check(self):
         try:
-            self.values['name'] = str(self.values['name'])
-            self.values['threads'] = max(1, int(self.values['threads']))
-            self.values['memory'] = max(1, int(self.values['memory']))
-            self.values['gpus'] = max(0, int(self.values['gpus']))
-            self.values['hours'] = max(1, int(self.values['hours']))
-            self.values['subtasks'] = max(1, int(self.values['subtasks']))
-        except:
-            print('Parser: Invalid block definition. Abort.')
+            for key in ['name', 'threads', 'memory', 'gpus', 'hours', 'subtasks']:
+                original_value = self.values[key]
+                if isinstance(self.values[key], int):
+                    self.values[key] = max(1, self.values[key])
+                elif key == 'gpus':
+                    self.values[key] = max(0, self.values[key])
+                else:
+                    self.values[key] = str(self.values[key])
+                print(f"Checked {key}: converted from {original_value} to {self.values[key]}")
+        except Exception as e:
+            print(f'Parser: Invalid block definition. Key {key} with value {self.values[key]} caused error: {str(e)}. Abort.')
             exit()
 
     def exists(self, key):
-        return key in self.values.keys()
+        return key in self.values
 
     def set_value(self, key, value):
-        self.values[str(key)] = value
+        self.values[key] = value
+        print(f"Set {key} to {value}")
 
     def get_value(self, key):
-        assert (self.exists(str(key)))
-        return self.values[str(key)]
-
+        assert self.exists(key), f"Key {key} does not exist"
+        return self.values[key]
 
 class Block_Parser(object):
-
     def __init__(self):
         self.blocks = []
 
     def _parse_block(self, block):
-        # convert gpu=True/gpu=False to gpus=1/gpus=0 for compatibility with old scripts
         block[0] = re.sub('gpu=true', 'gpus=1', block[0])
         block[0] = re.sub('gpu=false', 'gpus=0', block[0])
-        # extract job parameters
         meta = re.sub('\\).*', '', re.sub('.*\\(', '', block[0])).split(',')
         parsed_block = Block()
         for param in meta:
-            if len(param.split('=')) == 2:
-                key = param.split('=')[0].replace(' ', '')
-                value = param.split('=')[1].replace(' ', '')
-                if parsed_block.exists(key):
-                    parsed_block.set_value(key, value)
-        # create job scripts
+            key, value = param.split('=')
+            key = key.strip()
+            value = value.strip()
+            if parsed_block.exists(key):
+                parsed_block.set_value(key, value)
         parsed_block.set_value('script', block[1:])
-        # check block
         parsed_block.check()
         return parsed_block
 
@@ -66,27 +63,26 @@ class Block_Parser(object):
             f = open(script, 'r')
             content = f.read().split('\\n')
             f.close()
-        except:
-            print('Parser: script %s could not be opened.' % script)
+        except Exception as e:
+            print(f'Parser: script {script} could not be opened. Error: {str(e)}')
             exit()
-        # extract blocks from script
+
         block_starts = []
         for i, line in enumerate(content):
-            if line[0:6] == '#block':
+            if line.startswith('#block'):
                 block_starts.append(i)
-        block_starts.append(len(content)) # indicate end of file
+        block_starts.append(len(content))
         blocks = []
         for i in range(len(block_starts) - 1):
-            blocks.append( content[block_starts[i]:block_starts[i+1]] )
-        # at least one block has to exist
-        if len(blocks) == 0:
-            print('Error: No block defined in %s' % script)
+            blocks.append(content[block_starts[i]:block_starts[i + 1]])
+        if not blocks:
+            print(f'Error: No block defined in {script}')
             exit()
-        # parse each single block
         parsed_blocks = []
         for block in blocks:
-            parsed_blocks.append( self._parse_block(block) )
+            parsed_blocks.append(self._parse_block(block))
         return parsed_blocks
 
-#p = Block_Parser()
-#p.parse('script.sh')
+# Uncomment to test parsing
+# p = Block_Parser()
+# p.parse('script.sh')
